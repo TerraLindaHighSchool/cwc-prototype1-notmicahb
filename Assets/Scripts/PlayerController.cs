a40176df38b46e9089073ff6e2a00ea580be8404
notmicahb <60672481+notmicahb@users.noreply.github.com>
2020-12-01 15:21:59 -0800

    Unit 1 Complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,7 +19,7 @@ public class PlayerController : MonoBehaviour
         //Move the vehicle forward
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
-        //Moves the car forward based on vertical input
+        //Moves the car forward based on vertical input.
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         //Rotates the car based on horizontal input
         transform.Rotate(Vector3.up * horizontalInput * turnSpeed * Time.deltaTime);
